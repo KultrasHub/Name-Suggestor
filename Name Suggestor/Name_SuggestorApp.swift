77fde5f03b7627dc3cc0913f7373be04afb51720
KultrasHub <s3863956@rmit.edu.vn>
2022-09-07 21:39:48 +0700

    Initial Commit

---
+++ Name Suggestor/Name_SuggestorApp.swift
@@ -0,0 +1,20 @@
+//
+//  Name_SuggestorApp.swift
+//  Name Suggestor
+//
+//  Created by Khoa on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Name_SuggestorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
