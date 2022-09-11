8b0e48a2f7f81d8db5638384c3d132b11de96190
KultrasHub <s3863956@rmit.edu.vn>
2022-09-11 20:30:38 +0700

    Sign In, Sign Up

---
+++ Name Suggestor/Components/Button.swift
@@ -0,0 +1,49 @@
+//
+//  Button.swift
+//  Name Suggestor
+//
+//  Created by Khoa on 11/09/2022.
+//
+
+import SwiftUI
+
+struct MyButton: View {
+    let content:String
+    var body: some View {
+        Button{}
+        label:{
+            RoundedRectangle(cornerRadius: 40)
+                .fill(.white)
+                .overlay(Text(content.uppercased())
+                    .foregroundColor(MyColor.background)
+                    .font(.system(size: 20).weight(.bold)))
+                .frame(width: MySize.width * 0.8, height: MySize.height * 0.06, alignment: .center)
+        }
+    }
+}
+struct MyBorderButton: View {
+    let content:String
+    var body: some View {
+        Button{}
+        label:{
+            RoundedRectangle(cornerRadius: 40)
+                .stroke(.white,lineWidth: 2)
+                .overlay(Text(content.uppercased())
+                    .foregroundColor(.white)
+                    .font(.system(size: 20).weight(.bold)))
+                .frame(width: MySize.width * 0.8, height: MySize.height * 0.06, alignment: .center)
+        }
+    }
+}
+
+struct Button_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack{
+            Background()
+            VStack{
+        MyButton(content: "Sign In")
+                MyBorderButton(content: "Sign Up")
+            }
+        }
+    }
+}
